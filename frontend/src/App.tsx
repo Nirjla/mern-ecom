f51e332bdba330543050dd7e1622eadc6c103f12
Nirjla <shakyanirjala6@gmail.com>
2024-07-31 19:38:55 +0545

    sidebarlink comp made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { Routes, useLocation } from 'react-router-dom'
 import './App.css'
+import { MainLayout } from './components/layout/MainLayout'
+import { useEffect, useState } from 'react'
+import Loader from './components/common/Loader';
 
 function App() {
-  return (
-    <></>
+  const [loading, setLoading] = useState(true);
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]); //setup an effect to scroll the window to the top whenever the pathname changes
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />) : (
+    <MainLayout>
+      <Routes>
+        {/* <Route index /> */}
+      </Routes>
+    </MainLayout>
   )
 }
 
--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,5 +1,18 @@
-export const MainLayout = () => {
+import { ReactNode, useState } from "react"
+import { Sidebar } from "../features/SideBar/SideBar"
+
+export const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+      const [sidebarOpen, setSidebarOpen] = useState(false)
       return (<>
-      
+            <div className="dark:bg-boxdark-2 dark:text-bodydark">
+                  <div className="flex h-screen overflow-hidden">
+                        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                        <main>
+                              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                                    {children}
+                              </div>
+                        </main>
+                  </div>
+            </div>
       </>)
 }
