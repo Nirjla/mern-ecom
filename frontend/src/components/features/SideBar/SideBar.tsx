965a284d35772621401d7a4c93343b80783d41d0
Nirjla <shakyanirjala6@gmail.com>
2024-07-31 17:25:01 +0545

    tailwind setup

---
+++ frontend/src/components/features/SideBar/SideBar.tsx
@@ -0,0 +1,8 @@
+export const Sidebar = () => {
+      return (<>
+            <aside
+                  className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
+            >
+            </aside>
+      </>)
+}
