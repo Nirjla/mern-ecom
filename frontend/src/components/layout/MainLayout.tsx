057f9dff509ff09fcb591009d7c6b364e868a609
Nirjla <shakyanirjala6@gmail.com>
2024-08-01 19:07:18 +0545

    admin- frontend

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from "react"
 import { Sidebar } from "../features/SideBar/SideBar"
+import { Header } from "../features/Header/Header"
 
 export const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
       const [sidebarOpen, setSidebarOpen] = useState(false)
@@ -7,11 +8,15 @@ export const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
             <div className="dark:bg-boxdark-2 dark:text-bodydark">
                   <div className="flex h-screen overflow-hidden">
                         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
                         <main>
                               <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                                     {children}
                               </div>
                         </main>
+                              </div>
                   </div>
             </div>
       </>)
